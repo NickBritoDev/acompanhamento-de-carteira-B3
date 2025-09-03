ead0f9affebd31133edf962a8de221edd9206de8
Brito <101473345+NickBritoDev@users.noreply.github.com>
2025-09-03 11:20:41 -0300

    feat: nao subindo a carteira original

--- src/data/carteira.js
+++
@@ -1,8 +0,0 @@
-export const carteira = [
-  { "ativo": "ITSA4.SA", "nome": "ITSA4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 5 }, 
-  { "ativo": "SAPR4.SA", "nome": "SAPR4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 5 }, 
-  { "ativo": "SPXS11.SA", "nome": "SPXS11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 3 }, 
-  { "ativo": "KCRE11.SA", "nome": "KCRE11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 3 }, 
-  { "ativo": "MXRF11.SA", "nome": "MXRF11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 3 }, 
-  { "ativo": "GARE11.SA", "nome": "GARE11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 3 } 
-]
