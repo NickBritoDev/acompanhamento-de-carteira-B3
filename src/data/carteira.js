f5267cbc8ec4c550ec01d2a7fc2cc664434a5417
Brito <101473345+NickBritoDev@users.noreply.github.com>
2025-09-03 14:16:50 -0300

    fix: subindo o data para o build

---
+++ src/data/carteira.js
@@ -0,0 +1,11 @@
+export const carteira = [
+  { "ativo": "BBAS3.SA", "nome": "BBAS3", "tipo": "Ação", "qtdAtual": 1, "qtdMeta": 40 },
+  { "ativo": "ITSA4.SA", "nome": "ITSA4", "tipo": "Ação", "qtdAtual": 2, "qtdMeta": 40 },
+  { "ativo": "ITUB4.SA", "nome": "ITUB4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 35 },
+  { "ativo": "TAEE11.SA", "nome": "TAESA", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 35 },
+  { "ativo": "SAPR4.SA", "nome": "SAPR4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 40 },
+  { "ativo": "SPXS11.SA", "nome": "SPXS11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 },
+  { "ativo": "KCRE11.SA", "nome": "KCRE11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 },
+  { "ativo": "MXRF11.SA", "nome": "MXRF11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 },
+  { "ativo": "GARE11.SA", "nome": "GARE11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 }
+]
