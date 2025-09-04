8b12fa84bd6745a0191cfa52bc6b32e33623009d
Brito <101473345+NickBritoDev@users.noreply.github.com>
2025-09-03 21:09:58 -0300

    feat: ajustes no cron para timezone br

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import cron from "node-cron";
 import { enviarEmail } from "./email/enviar-email.js";
 import { atualizarCarteira } from "./functions/atualizar-carteira.js";
 
-cron.schedule("0 10,16 * * *", async () => {
-    console.log("Iniciando atualização e envio de email:", new Date().toLocaleString());
+cron.schedule(
+  "0 10,16 * * *",
+  async () => {
+    const agora = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+    console.log("Iniciando atualização e envio de email:", agora);
     try {
-        const dados = await atualizarCarteira();
-        await enviarEmail(dados);
-        console.log("Email enviado com sucesso!");
+      const dados = await atualizarCarteira();
+      await enviarEmail(dados);
+      console.log("Email enviado com sucesso!");
     } catch (error) {
-        console.error("Erro ao atualizar carteira ou enviar email:", error);
+      console.error("Erro ao atualizar carteira ou enviar email:", error);
     }
-});
-
+  },
+  {
+    timezone: "America/Sao_Paulo",
+  }
+);
 
 (async () => {
-    const dados = await atualizarCarteira();
-    await enviarEmail(dados);
+  const dados = await atualizarCarteira();
+  await enviarEmail(dados);
 })();
