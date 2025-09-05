62d7d82b3ba69846c8c5ce69fdcf9608429a0b04
Brito <101473345+NickBritoDev@users.noreply.github.com>
2025-09-05 15:29:23 -0300

    feat: ajuste de carteira

--- src/data/carteira.js
+++ src/data/carteira.js
@@ -1,16 +1,17 @@
 export const carteira = [
-  { "ativo": "BBAS3.SA", "nome": "BBAS3", "tipo": "Ação", "qtdAtual": 1, "qtdMeta": 20 }, // att meta para 50 assim que bater as 30
-  { "ativo": "ITSA4.SA", "nome": "ITSA4", "tipo": "Ação", "qtdAtual": 2, "qtdMeta": 30 }, // att meta para 50 assim que bater as 30
-  // { "ativo": "BBDC4.SA", "nome": "BBDC4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 20 }, // att meta para 50 assim que bater as 30
-  { "ativo": "TAEE4.SA", "nome": "TAESA", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 30 }, // att meta para 10 assim que bater as 5
-  { "ativo": "CXSE3.SA", "nome": "CXSE3", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 25 }, // att meta para 50 assim que bater as 30
-  { "ativo": "SPXS11.SA", "nome": "SPXS11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 30 }, // att meta para 50 assim que bater as 25
-  { "ativo": "KCRE11.SA", "nome": "KCRE11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 }, // att meta para 50 assim que bater as 25
-  { "ativo": "MXRF11.SA", "nome": "MXRF11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 }, // att meta para 50 assim que bater as 25
-  { "ativo": "GARE11.SA", "nome": "GARE11", "tipo": "FII", "qtdAtual": 2, "qtdMeta": 25 } // att meta para 50 assim que bater as 25
+  { "ativo": "BBAS3.SA", "nome": "BBAS3", "tipo": "Ação", "qtdAtual": 1, "qtdMeta": 1 }, // att meta para 50 assim que bater as 30
+  { "ativo": "ITSA4.SA", "nome": "ITSA4", "tipo": "Ação", "qtdAtual": 25, "qtdMeta": 25 }, // att meta para 50 assim que bater as 25
+  { "ativo": "TAEE4.SA", "nome": "TAESA", "tipo": "Ação", "qtdAtual": 25, "qtdMeta": 25 }, // att meta para 10 assim que bater as 5
+  { "ativo": "SAPR4.SA", "nome": "SAPR4", "tipo": "Ação", "qtdAtual": 25, "qtdMeta": 25 }, // att meta para 50 assim que bater as 30
+  { "ativo": "CMIG4.SA", "nome": "CMIG4", "tipo": "Ação", "qtdAtual": 25, "qtdMeta": 25 }, // att meta para 50 assim que bater as 30
+  { "ativo": "SPXS11.SA", "nome": "SPXS11", "tipo": "FII", "qtdAtual": 30, "qtdMeta": 30 }, // att meta para 50 assim que bater as 25
+  { "ativo": "KCRE11.SA", "nome": "KCRE11", "tipo": "FII", "qtdAtual": 30, "qtdMeta": 25 }, // att meta para 50 assim que bater as 25
+  { "ativo": "MXRF11.SA", "nome": "MXRF11", "tipo": "FII", "qtdAtual": 30, "qtdMeta": 25 }, // att meta para 50 assim que bater as 25
+  { "ativo": "GARE11.SA", "nome": "GARE11", "tipo": "FII", "qtdAtual": 30, "qtdMeta": 25 } // att meta para 50 assim que bater as 25
 ]
+
 // { "ativo": "WEGE3.SA", "nome": "WEGE3", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 25 },  // proxima rodada de aportes talvez
 // { "ativo": "XPLG11.SA", "nome": "XPLG11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 25 }, // proxima rodada de aportes talvez
-// { "ativo": "ITUB4.SA", "nome": "ITUB4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 5 }, // proxima rodada de aportes talvez
-// { "ativo": "HGLG11.SA", "nome": "HGLG11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 5 } // proxima rodada de aportes talvez
-// { "ativo": "SAPR4.SA", "nome": "SAPR4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 25 }, // proxima rodada de aportes certeza
+// { "ativo": "ITUB4.SA", "nome": "ITUB4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 5 },   // proxima rodada de aportes talvez
+// { "ativo": "HGLG11.SA", "nome": "HGLG11", "tipo": "FII", "qtdAtual": 0, "qtdMeta": 5 }   // proxima rodada de aportes talvez
+// { "ativo": "BBDC4.SA", "nome": "BBDC4", "tipo": "Ação", "qtdAtual": 0, "qtdMeta": 20 },  // proxima rodada de aportes talvez
